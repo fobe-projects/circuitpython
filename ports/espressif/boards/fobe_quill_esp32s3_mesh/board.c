46c6428b32178ccc70a30af1e6ccb85a66d8e05a
Chiho Sin <chihosin@icloud.com>
2025-09-04 20:56:57 +0800

    boards: Add support for FoBE Quill ESP32S3 Mesh
    This commit introduces the necessary files and configurations for the FoBE Quill ESP32S3 Mesh board, including GPIO setup, pin definitions, and basic board initialization routines.
    
    Signed-off-by: Chiho Sin <chihosin@icloud.com>

---
+++ ports/espressif/boards/fobe_quill_esp32s3_mesh/board.c
@@ -0,0 +1,26 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2020 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#include "supervisor/board.h"
+#include "mpconfigboard.h"
+#include "shared-bindings/microcontroller/Pin.h"
+#include "driver/gpio.h"
+
+void board_init(void) {
+    reset_board();
+}
+
+void reset_board(void) {
+    // Turn on MFP power by default.
+    gpio_set_direction(1, GPIO_MODE_DEF_OUTPUT);
+    gpio_set_level(1, true);
+
+    // Turn on DISP power by default.
+    gpio_set_direction(12, GPIO_MODE_DEF_OUTPUT);
+    gpio_set_level(12, true);
+}
+
+// Use the MP_WEAK supervisor/shared/board.c versions of routines not defined here.
